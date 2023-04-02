b7863dfcfe14a57b21673c46572c47e2bfa3e82f
lzugaj <luka.zugaj@sigit.hr>
2023-04-02 19:18:22 +0200

    Setup testcontainers

---
+++ src/test/java/com/luv2code/health/tracker/integration/AbstractIntegrationTest.java
@@ -0,0 +1,32 @@
+package com.luv2code.health.tracker.integration;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.TestPropertySourceUtils;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@ContextConfiguration(initializers = AbstractIntegrationTest.DataSourceInitializer.class)
+public abstract class AbstractIntegrationTest {
+
+    @Container
+    private static final PostgreSQLContainer<?> database = new PostgreSQLContainer<>("postgres:latest");
+
+    public static class DataSourceInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(@NotNull ConfigurableApplicationContext applicationContext) {
+            TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
+                    applicationContext,
+                    "spring.test.database.replace=none",
+                    "spring.datasource.url=" + database.getJdbcUrl(),
+                    "spring.datasource.username=" + database.getUsername(),
+                    "spring.datasource.password=" + database.getPassword()
+            );
+        }
+    }
+}
